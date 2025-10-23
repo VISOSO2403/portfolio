012af530dc73bc92d4d0fe2c3cc332720fa942a6
Oliver Visoso <ovisosodev@gmail.com>
2025-10-23 15:44:59 -0600

    add sections in home

---
+++ src/data/badges.ts
@@ -0,0 +1,32 @@
+export const badges = [
+  {
+    name: "JavaScript",
+    imageUrl: "/icons/javascript.svg",
+    alt: "JavaScript Badge",
+  },
+  {
+    name: "TypeScript",
+    imageUrl: "/icons/typescript.svg",
+    alt: "TypeScript Badge",
+  },
+  {
+    name: "Astro",
+    imageUrl: "/icons/astro-icon-light.svg",
+    alt: "Astro Badge",
+  },
+  {
+    name: "Tailwind CSS",
+    imageUrl: "/icons/tailwindcss.svg",
+    alt: "Tailwind CSS Badge",
+  },
+  {
+    name: "Git",
+    imageUrl: "/icons/git.svg",
+    alt: "Git Badge",
+  },
+  {
+    name: "Expo",
+    imageUrl: "/icons/expo.svg",
+    alt: "Expo Badge",
+  },
+];
